a3424eced13329f05fe6f66dcf2bb0f2c49fd8a8
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2016-09-14 01:49:37 -0400

    Create randomizer.py

---
+++ randomizer.py
@@ -0,0 +1,20 @@
+s (16 sloc)  1.16 KB
+import random
+
+CLASSES =  {
+    forth = [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson','Brian', 'Farhan', 'Janet', 'Harry', 'Kevin','Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth','Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj','Amy', 'Arvind', 'Nobel', 'Angela', 'Edward','Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],
+    eigth = ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki','Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda','Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vncent','Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel','Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio','Kelly', 'William', 'Jordan', 'Haley', 'Henry'],
+    ninth = ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C','Joel', 'Steph', 'Xinhui', 'Richard', 'Misha','Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry','Michael', 'Stiven', 'Will',  'Olivia', 'Constantine','Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L','Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
+} 
+
+def randomMaker(){
+	x = randint(0,2);
+	if x == 0
+	print(fourth[randint(0,len(fourth))])
+	elif x == 1
+	print(eigth[randint(0,len(eigth))])
+	elif x == 2
+	print(ninth[randint(0,len(ninth))])	
+
+	return
+}
